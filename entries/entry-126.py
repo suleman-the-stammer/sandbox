84e19b775b955f27972dbe02bdcf88a3824607c5
Sulaiman <stammer786@gmail.com>
2026-07-23 18:09:50 +0500

    Add entry 126

---
+++ entries/entry-126.py
@@ -0,0 +1,10 @@
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    for d in range(2, int(n**0.5) + 1):
+        if n % d == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    print([n for n in range(2, 30) if is_prime(n)])
